58342b404d54d883c16a152ffea203742c8334e6
Antoine Albertelli <antoinea101@gmail.com>
2013-02-27 16:30:48 +0100

    Oooops, forgot a rjmp

--- tp2/inout2/inout2.asm
+++ tp2/inout2/inout2.asm
@@ -11,6 +11,7 @@ loop:
     sbic    PIND, 0  ; wait for button 0 press 
     rjmp    PC-1
     sbis    PIND, 0 ; wait for button 0 release
+    rjmp    PC-1
 
     dec     r16
     out     PORTB, r16
